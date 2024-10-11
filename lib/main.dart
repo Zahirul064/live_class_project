b667a7832f9fc3d92df6f4eff3012babdd7f9afa
Zahirul <zahirul064@gmail.com>
2024-10-11 06:58:33 +0600

    new updated

--- lib/main.dart
+++ lib/main.dart
@@ -13,22 +13,35 @@ class HelloWorldApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Home',
-            style: TextStyle(
-              overflow: TextOverflow.ellipsis,
-              color: Colors.black,
+      title: 'Hello World App',
+      home: Home() ,
+    );
+  }
+}
 
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.green.shade100,
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.green ,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green,
+              foregroundColor: Colors.white,
+                textStyle: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+                onPressed: () {}, child: Text('Tab'))
+          ],
         ),
-
-
-
       ),
     );
   }
