efcc87baf84fe84e2cc54c0e9e1e9e91729c13de
Zahirul <zahirul064@gmail.com>
2024-10-22 22:52:50 +0600

    new updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-// Image(asset, network), Column, Row
-
-// Divider, ModelBottomSheet, TextField
+// Drawer, NavigationBar, Bottom Navigation bar
 
 void main() {
   runApp(HelloWorldApp());
@@ -29,98 +27,21 @@ class Home extends StatelessWidget {
         title: Text('Home'),
         backgroundColor: Colors.green,
       ),
-      body: Center(
+      drawer: Drawer(
+        elevation: 0,
+        shadowColor: Colors.red,
+
+        backgroundColor: Colors.amber.shade50,
+        shape: OutlineInputBorder(
+
+        ),
+        width: 300,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 100),
-            ElevatedButton(
-              onPressed: () {
-                //showAboutDialog(context: context);
-                showModalBottomSheet(
-                    //barrierColor: Colors.tealAccent,
-                    backgroundColor: Colors.amberAccent[50],
-                    isScrollControlled: true,
-                    useSafeArea: true,
-                    enableDrag: true,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    context: context,
-                    builder: (ctx) {
-                      return Column(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(vertical: 16),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Title',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Divider(
-                            height: 20,
-                            thickness: 4,
-                          ),
-                          Text('sample'),
-                          Row(
-                            children: [
-                              ElevatedButton(
-                                  onPressed: () {}, child: Text('Save')),
-                              ElevatedButton(
-                                  onPressed: () {}, child: Text('Cancel'))
-                            ],
-                          )
-                        ],
-                      );
-                    });
-              },
-              child: Text('Show dialog'),
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: TextField(
-                maxLength: 50,
-                onChanged: (String? value) {
-                  print(value);
-                },
-                controller: TextEditingController(),
-                maxLines: 1,
-                keyboardType: TextInputType.phone,
-                enabled: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red)),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green, width: 2)),
-                  disabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey, width: 4)
-                  ),
-                  hintText: 'Phone',
-                  hintStyle: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    color: Colors.grey.shade400,
-                  ),
-                  labelText: 'Phone Number',
-                  prefixIcon: Icon(Icons.phone),
-                  suffixIcon: Icon(Icons.person),
-                ),
-                //obscureText: true,
-              ),
-            ),
+            Text("data")
           ],
         ),
-      ),
+      )
     );
   }
 }
