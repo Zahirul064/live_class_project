091b94b9c8f2602dfe266acc3b8390b883c0daa4
Zahirul <zahirul064@gmail.com>
2024-10-30 21:13:12 +0600

    new updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 // Gridview, ListTile, Form, GolablKey, Key hh
 
 void main() {
@@ -43,6 +41,9 @@ class Home extends StatelessWidget {
     'Zarifa',
     'Zarifa'
   ];
+  TextEditingController _emailTEController = TextEditingController();
+  TextEditingController _passwordTEController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -51,12 +52,82 @@ class Home extends StatelessWidget {
         title: Text('ListTile Example'),
         backgroundColor: Colors.green,
       ),
-      body: ListView.builder(
-          itemCount: friendList.length,
+
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _emailTEController,
+                decoration: InputDecoration(hintText: 'Email'),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter your Email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordTEController,
+                decoration: InputDecoration(hintText: 'Password'),
+                validator: (String? value){
+                  if(value?.isEmpty?? true){
+                    return 'Enter your Password';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    // String email = _emailTEController.text;
+                    // String pass = _passwordTEController.text;
+                    //
+                    // if (email.isNotEmpty && pass.isNotEmpty) {
+                    //   print("Login Success");
+                    // } else {
+                    //   print("Login failed. Missing data");
+                    // }
+                    if(_formKey.currentState!.validate()){
+                      print("Login Success");
+                    }
+                  },
+                  child: Text('Tap'))
+            ],
+          ),
+        ),
+      ),
+      /*body: GridView.builder(
+        itemCount: friendList.length,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
           itemBuilder: (context, index) {
+          return Center(
+              child: Text(
+                  friendList[index], style: TextStyle(color: Colors.pink,)),
+
+          );
+          }
+          ),*/
+
+      /*body: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, mainAxisSpacing: 10),
+          children: [
+            Text('data'),
+            Text('data'),
+            Text('data'),
+            Text('data'),
+          ],
+        )*/
+        /*body: ListView.builder(
+          itemCount: friendList.length,
+          itemBuilder: (BuildContext context, int index) {
             return ListTile(
               title: Text(friendList[index]),
-              subtitle: Text('frind no $index'),
+              //subtitle: Text('frind no $index'),
+              subtitle: Text('Friend no ${index+1}'),
               trailing: Icon(Icons.arrow_forward_ios),
               leading: CircleAvatar(child: Icon(Icons.account_circle_rounded,size: 30,)),
               onTap: () {print('on Tab $index');},
@@ -69,15 +140,15 @@ class Home extends StatelessWidget {
               //dense: false,
             );
 
-            /*Padding(
+            */ /*Padding(
               padding: const EdgeInsets.all(16.0),
               child: Text(
                 friendList[index],style: TextStyle(
                 fontSize: 16
               ),
               ),
-            );*/
-          }),
-    );
+            );*/ /*
+          }),*/
+        );
   }
 }
