86c005e2343750c45ff94eb86062822da95220a5
Zahirul <zahirul064@gmail.com>
2024-11-22 23:16:27 +0600

    new updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-//  MediaQuery | Wrap | LayoutBuilder | OrientationBuilder
-
-
+// Aspect Ration | Expanded |Flexible| ReactionllySizeBox
+//
 
 import 'package:flutter/material.dart';
 
@@ -20,152 +19,99 @@ class SimpleApp extends StatelessWidget {
   }
 }
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
   Widget build(BuildContext context) {
-    //Size screenSize = MediaQuery.of(context).size;
-    Size screenSize = MediaQuery.sizeOf(context);
-    print(screenSize.width);
-    print(screenSize.height);
-    print(screenSize.flipped);
-    print(screenSize.aspectRatio);
-    print(screenSize.longestSide);
-    print(screenSize.shortestSide);
-
-    print(MediaQuery.of(context).devicePixelRatio);
-    print(MediaQuery.of(context).orientation);
-
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Media Query')),
-      ),
-      // body: Center(
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: [
-      //       Wrap(
-      //         alignment: WrapAlignment.center,
-      //         crossAxisAlignment: WrapCrossAlignment.start,
-      //         spacing: 10,
-      //         runSpacing: 16,
-      //         children: [
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //           TextButton(
-      //             onPressed: () {},
-      //             child: Text('Go to Profile'),
-      //           ),
-      //         ],
-      //       ),
-      //     ],
-      //   ),
+      // appBar: AppBar(
+      //   title: Text('Home'),
       // ),
-      // body: Padding(
-      //   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      //   child: LayoutBuilder(
-      //     builder: (BuildContext context, BoxConstraints constraints) {
-      //       return Center(
-      //         child: Text('${constraints.maxWidth}, ${constraints.maxHeight}'),
-      //       );
-      //     }
+
+      // body: FractionallySizedBox(
+      //   heightFactor: .5,
+      //   widthFactor: 1,
+      //   alignment: FractionalOffset.center,
+      //   child: DecoratedBox(
+      //     decoration: BoxDecoration(
+      //       border: Border.all(color: Colors.blue, width: 10),
+      //     ),
       //   ),
       // ),
 
-      body: OrientationBuilder(
-        builder: (context, orientation) {
-          if (orientation == Orientation.portrait) {
-            return Center(
-              child: Text('portrait'),
-            );
-          } else {
-            return Center(child: Text('Landscrep'));
-          }
-        },
+      // body: AspectRatio(
+      //   aspectRatio: 16/9,
+      //   child: ColoredBox(color: Colors.black87,),
+      // )
+
+/*      body: Column(
+        children: [
+          Flexible(
+            flex: 3,
+            fit: FlexFit.tight,
+            child: SizedBox(
+              width: double.maxFinite,
+              child: ColoredBox(
+                color: Colors.red,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: SizedBox(
+              width: double.maxFinite,
+              child: ColoredBox(
+                color: Colors.orange,
+              ),
+            ),
+          ),
+          Flexible(
+            fit: FlexFit.loose,
+            flex: 1,
+            child: SizedBox(
+              width: double.maxFinite,
+              height: 40,
+              child: ColoredBox(
+                color: Colors.black,
+              ),
+            ),
+          ),
+
+        ],
+      ),*/
+
+      body: SafeArea(
+        top: true,
+        bottom: false,
+        child: Tooltip(
+          message: 'This is Row',
+          showDuration:Duration(seconds: 2),
+          triggerMode: TooltipTriggerMode.tap,
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.orange,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  color: Colors.black,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.pink,
+                  height: 100,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
